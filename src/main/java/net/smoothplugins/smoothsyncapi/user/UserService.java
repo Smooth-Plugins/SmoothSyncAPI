6d018820955018a24f5f4a73f1081e85c1daffb3
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-19 18:59:24 +0200

    Add user service

---
+++ src/main/java/net/smoothplugins/smoothsyncapi/user/UserService.java
@@ -0,0 +1,121 @@
+package net.smoothplugins.smoothsyncapi.user;
+
+import net.smoothplugins.smoothsyncapi.service.Destination;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+
+    /**
+     * Creates a user. The user will be saved in the database, not in the cache.
+     * @param user
+     */
+    void create(User user);
+
+    /**
+     * Updates a user.
+     * Use Destination to specify where the user should be updated.
+     * @param user
+     * @param destinations
+     */
+    void update(User user, Destination... destinations);
+
+    /**
+     * Checks if a user exists by their UUID (preferred over username).
+     * @param uuid
+     * @return true if the user exists, false if not.
+     */
+    boolean containsByUUID(UUID uuid);
+
+    /**
+     * Checks if a user exists by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @return true if the user exists, false if not.
+     */
+    boolean containsByUsername(String username);
+
+    /**
+     * Gets a (optional) user by their UUID (preferred over username).
+     * @param uuid
+     * @return the user if found, empty if not.
+     */
+    Optional<User> getUserByUUID(UUID uuid);
+
+    /**
+     * Gets a (optional) user by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @return the user if found, empty if not.
+     */
+    Optional<User> getUserByUsername(String username);
+
+    /**
+     * Deletes a user by their UUID (preferred over username).
+     * @param uuid
+     * @param destinations
+     */
+    void deleteByUUID(UUID uuid, Destination... destinations);
+
+    /**
+     * Deletes a user by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @param destinations
+     */
+    void deleteByUsername(String username, Destination... destinations);
+
+    /**
+     * Checks if a user exists in the cache by their UUID (preferred over username).
+     * @param uuid
+     * @return true if the user exists in the cache, false if not.
+     */
+    boolean cacheContainsByUUID(UUID uuid);
+
+    /**
+     * Checks if a user exists in the cache by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @return true if the user exists in the cache, false if not.
+     */
+    boolean cacheContainsByUsername(String username);
+
+    /**
+     * Load a user to the cache.
+     * @param user
+     */
+    void loadToCache(User user);
+
+    /**
+     * Remove the time to live from a user in the cache by their UUID (preferred over username).
+     * @param uuid
+     * @return true if the user was found and the time to live was removed, false if the user was not found.
+     */
+    boolean removeTTLFromCacheByUUID(UUID uuid);
+
+    /**
+     * Remove the time to live from a user in the cache by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @return true if the user was found and the time to live was removed, false if the user was not found.
+     */
+    boolean removeTTLFromCacheByUsername(String username);
+
+    /**
+     * Set the time to live of a user in the cache by their UUID (preferred over username).
+     * @param uuid
+     * @param seconds
+     * @return true if the user was found and the time to live was set, false if the user was not found.
+     */
+    boolean setTTLOfCacheByUUID(UUID uuid, int seconds);
+
+    /**
+     * Set the time to live of a user in the cache by their username.
+     * The username is case-insensitive.
+     * @param username
+     * @param seconds
+     * @return true if the user was found and the time to live was set, false if the user was not found.
+     */
+    boolean setTTLOfCacheByUsername(String username, int seconds);
+}
