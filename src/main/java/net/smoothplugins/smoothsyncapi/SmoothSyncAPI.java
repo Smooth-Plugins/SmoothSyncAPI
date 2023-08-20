c8d3ac1b04a2924062f9591c4ab6c33d3a35d1b2
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-20 22:25:36 +0200

    Update SmoothSyncAPI

--- src/main/java/net/smoothplugins/smoothsyncapi/SmoothSyncAPI.java
+++ src/main/java/net/smoothplugins/smoothsyncapi/SmoothSyncAPI.java
@@ -1,8 +1,10 @@
 package net.smoothplugins.smoothsyncapi;
 
 import net.smoothplugins.smoothsyncapi.user.UserService;
+import net.smoothplugins.smoothsyncapi.user.UserTranslator;
 
 public interface SmoothSyncAPI {
 
     UserService getUserService();
+    UserTranslator getUserTranslator();
 }
--- src/main/java/net/smoothplugins/smoothsyncapi/user/UserTranslator.java
+++ src/main/java/net/smoothplugins/smoothsyncapi/user/UserTranslator.java
@@ -3,7 +3,7 @@ package net.smoothplugins.smoothsyncapi.user;
 import org.bukkit.entity.Player;
 
 public interface UserTranslator {
-    
+
     User translateToUser(User user, Player player);
     Player translateToPlayer(User user, Player player);
 }
